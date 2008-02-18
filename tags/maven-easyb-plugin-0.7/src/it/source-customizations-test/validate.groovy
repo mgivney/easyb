9e128f3e9c729162d45a14769c7cab09f7877e23
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-02-18 23:32:16 +0000

    Removed tag for maven plugin 0.7 release since it failed because a distribution target wasn't configured

--- tags/maven-easyb-plugin-0.7/src/it/source-customizations-test/validate.groovy
+++
@@ -1,7 +0,0 @@
-// Verify behavior report
-behaviorReport = new File("${basedir}/target/easyb/report.xml")
-assert behaviorReport.exists()
-
-def results = new XmlParser().parse(behaviorReport)
-assert '8' == results.'@totalrun'
-assert '0' == results.'@totalfailed'
