54bd4d2c94c5c40ac292785c00651990d3375975
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-04-22 04:11:00 +0000

    [maven-release-plugin]  copy for tag maven-parent-0.8.1

--- intellij-plugins/easyb-plugin/src/main/java/org/easyb/plugin/ui/EasybView.java
+++
@@ -1,8 +0,0 @@
-package org.easyb.plugin.ui;
-
-import org.easyb.plugin.ui.swing.EasybTreeNode;
-
-public interface EasybView {
-    void addBehaviorResult(EasybTreeNode resultNode);
-    void refresh();
-}
