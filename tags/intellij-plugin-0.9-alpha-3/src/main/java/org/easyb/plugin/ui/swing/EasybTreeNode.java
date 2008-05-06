f0daf8e1718730db0e885a023840ede7d0375ffe
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-05-06 14:22:45 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-0.9-alpha-3

---
+++ tags/intellij-plugin-0.9-alpha-3/src/main/java/org/easyb/plugin/ui/swing/EasybTreeNode.java
@@ -0,0 +1,59 @@
+package org.easyb.plugin.ui.swing;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.easyb.plugin.StepResult;
+
+public class EasybTreeNode extends DefaultMutableTreeNode {
+    private StepResult result;
+
+    public EasybTreeNode(StepResult result) {
+        super(result);
+        this.result = result;
+    }
+
+    public StepResult getResult() {
+        return result;
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        EasybTreeNode that = (EasybTreeNode) other;
+
+        if (getUserObject().equals(that.getUserObject())) {
+            if (getChildCount() != that.getChildCount()) {
+                return false;
+            }
+            for (int i = 0; i < getChildCount(); i++) {
+                if (!getChildAt(i).equals(that.getChildAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        return getUserObject().hashCode();
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(getUserObject()).append("[");
+        for (int i = 0; i < getChildCount(); i++) {
+            builder.append(getChildAt(i)).append(" ");
+        }
+        builder.append("]");
+
+        return builder.toString();
+    }
+}
