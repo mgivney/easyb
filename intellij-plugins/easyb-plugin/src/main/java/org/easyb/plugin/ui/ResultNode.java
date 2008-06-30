59753fedc32f5e8840c32b90d40ee57f42d189e3
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-06-30 01:35:23 +0000

    0.9 release on June 29 2008

--- intellij-plugins/easyb-plugin/src/main/java/org/easyb/plugin/ui/ResultNode.java
+++
@@ -1,9 +0,0 @@
-package org.easyb.plugin.ui;
-
-import org.easyb.plugin.StepResult;
-
-public interface ResultNode<T> {
-    StepResult getResult();
-
-    void add(T child);
-}
--- maven-plugins/maven-easyb-plugin/src/it/restrict-tests-to-run/validate.groovy
+++
@@ -1,8 +0,0 @@
-// Verify behavior report
-report = new File("${basedir}/target/easyb-report.xml")
-assert report.exists()
-
-def results = new XmlParser().parse(report)
-assert '2' == results.'@totalbehaviors'
-assert '0' == results.'@totalfailedbehaviors'
-
