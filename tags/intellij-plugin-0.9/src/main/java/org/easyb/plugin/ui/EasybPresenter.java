dd2007d36877f31f521463d4c98e9b73e0922557
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-06-30 16:38:21 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-0.9

---
+++ tags/intellij-plugin-0.9/src/main/java/org/easyb/plugin/ui/EasybPresenter.java
@@ -0,0 +1,80 @@
+package org.easyb.plugin.ui;
+
+import java.util.Stack;
+
+import org.disco.easyb.BehaviorStep;
+import org.disco.easyb.domain.Behavior;
+import org.disco.easyb.listener.ExecutionListener;
+import org.disco.easyb.result.Result;
+import static org.disco.easyb.util.BehaviorStepType.SPECIFICATION;
+import static org.disco.easyb.util.BehaviorStepType.STORY;
+import org.easyb.plugin.ConsoleOutputListener;
+import static org.easyb.plugin.Outcome.*;
+import org.easyb.plugin.StepResult;
+
+public class EasybPresenter <T extends ResultNode> implements ExecutionListener, ConsoleOutputListener {
+    private EasybView<T> view;
+    private NodeBuilder<T> nodeBuilder;
+    private Stack<T> nodeStack;
+    private boolean descendantFailed = false;
+
+    public EasybPresenter(EasybView<T> view, NodeBuilder<T> nodeBuilder) {
+        this.view = view;
+        this.nodeBuilder = nodeBuilder;
+        nodeStack = new Stack<T>();
+    }
+
+    public void startBehavior(Behavior behavior) {
+        descendantFailed = false;
+    }
+
+    public void startStep(BehaviorStep behaviorStep) {
+        T node = nodeBuilder.build(new StepResult(behaviorStep.getName(), behaviorStep.getStepType(), RUNNING));
+        if (behaviorStep.getStepType() == STORY || behaviorStep.getStepType() == SPECIFICATION) {
+            view.addBehaviorResult(node);
+        } else {
+            view.addBehaviorResult(nodeStack.peek(), node);
+        }
+        nodeStack.push(node);
+    }
+
+    public void describeStep(String s) {
+    }
+
+    public void gotResult(Result result) {
+        StepResult stepResult = nodeStack.peek().getResult();
+        if (descendantFailed) {
+            stepResult.setOutcome(FAILURE);
+        } else {
+            stepResult.setOutcome(outcomeForResult(result));
+        }
+        if (result.failed()) {
+            stepResult.setCause(result.cause);
+            descendantFailed = true;
+        }
+        view.refresh();
+    }
+
+    public void stopStep() {
+        StepResult stepResult = nodeStack.peek().getResult();
+        if (stepResult.getOutcome() == RUNNING) {
+            if (descendantFailed) {
+                stepResult.setOutcome(FAILURE);
+            } else {
+                stepResult.setOutcome(SUCCESS);
+            }
+        }
+        nodeStack.pop();
+        view.refresh();
+    }
+
+    public void stopBehavior(BehaviorStep behaviorStep, Behavior behavior) {
+    }
+
+    public void completeTesting() {
+    }
+
+    public void textAvailable(String text) {
+        view.writeConsole(text);
+    }
+}
---
+++ tags/intellij-plugin-0.9/src/test/groovy/org/easyb/plugin/ui/swing/SwingEasybViewHarness.groovy
@@ -0,0 +1,67 @@
+package org.easyb.plugin.ui.swing
+
+import groovy.ui.Console
+import javax.swing.JFrame
+import static org.disco.easyb.util.BehaviorStepType.*
+import org.disco.easyb.util.BehaviorStepType
+import org.easyb.plugin.Outcome
+import org.easyb.plugin.StepResult
+import org.easyb.plugin.ui.swing.SwingResultNode
+import org.easyb.plugin.ui.swing.SwingEasybView
+import javax.swing.tree.DefaultMutableTreeNode
+
+class SwingEasybViewHarness {
+    SwingEasybView view
+    DefaultMutableTreeNode storyNode;
+
+    public SwingEasybViewHarness() {
+        storyNode = nodeFor(STORY, "transferring funds", Outcome.RUNNING)
+    }
+
+    public void run() {
+        view = new SwingEasybView()
+        JFrame frame = new JFrame();
+        frame.add(view);
+        frame.show();
+        frame.setSize(600, 400);
+
+        view.addBehaviorResult(storyNode)
+        playStory()
+
+        Console console = new Console()
+        console.setVariable('frame', frame)
+        console.setVariable('view', view)
+        console.run()
+    }
+
+    public static void main(String[] args) {
+        new SwingEasybViewHarness().run();
+    }
+
+    private void playStory() {
+        SwingResultNode scenarioNode = nodeFor(SCENARIO, 'amount exceeds available funds', Outcome.RUNNING)
+        storyNode.add(scenarioNode)
+        view.refresh()
+
+        Thread.sleep(1000)
+        scenarioNode.add(nodeFor(GIVEN, 'an account balance of $100', Outcome.SUCCESS))
+        view.writeConsole 'an account balance of $100\n'
+        view.refresh()
+        Thread.sleep(1000)
+        scenarioNode.add(nodeFor(WHEN, 'a transfer of $150 is requested', Outcome.SUCCESS))
+        view.writeConsole 'a transfer of $150 is requested\n'
+        view.refresh()
+        Thread.sleep(1000)
+        scenarioNode.add(nodeFor(THEN, 'the request should be rejected', Outcome.FAILURE))
+        view.writeConsole 'the request should be rejected\n'
+        view.refresh()
+        Thread.sleep(1000)
+        scenarioNode.add(nodeFor(THEN, 'and not funds should be transfered', Outcome.PENDING))
+        view.writeConsole 'and not funds should be transfered\n'
+        view.refresh()
+    }
+
+    private static SwingResultNode nodeFor(BehaviorStepType type, String phrase, Outcome result) {
+        return new SwingResultNode(new StepResult(phrase, type, result));
+    }
+}
