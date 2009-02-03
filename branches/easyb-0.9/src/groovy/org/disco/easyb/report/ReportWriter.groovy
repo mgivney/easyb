f48680ffac0538d67d6789b35b406f2143cd12d3
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-02-03 22:17:15 +0000

    Branching easyb 0.9 to fix report issue

---
+++ branches/easyb-0.9/src/groovy/org/disco/easyb/report/ReportWriter.groovy
@@ -0,0 +1,10 @@
+package org.disco.easyb.report
+
+import org.disco.easyb.listener.ResultsCollector
+
+/**
+ * Common interface for easyb reports
+ */
+interface ReportWriter {
+    void writeReport(ResultsCollector results)
+}
---
+++ branches/easyb-0.9/src/java/org/disco/easyb/listener/ExecutionListener.java
@@ -0,0 +1,21 @@
+package org.disco.easyb.listener;
+
+import org.disco.easyb.BehaviorStep;
+import org.disco.easyb.domain.Behavior;
+import org.disco.easyb.result.Result;
+
+public interface ExecutionListener {
+    void startBehavior(Behavior behavior);
+
+    void startStep(BehaviorStep step);
+
+    void describeStep(String description);
+
+    void gotResult(Result result);
+
+    void stopStep();
+
+    void stopBehavior(BehaviorStep step, Behavior behavior);
+
+    void completeTesting();
+}
