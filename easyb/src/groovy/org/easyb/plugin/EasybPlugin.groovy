3cd48d621f9af50a4d91fed77de3c4520d8635e6
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-03-05 21:37:20 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-0.9.5

--- easyb/src/groovy/org/easyb/plugin/EasybPlugin.groovy
+++
@@ -1,9 +0,0 @@
-package org.easyb.plugin
-
-interface EasybPlugin {
-    String getName()
-
-    void beforeScenario(Binding binding)
-
-    void afterScenario(Binding binding)
-}
--- maven-plugins/maven-easyb-plugin/src/it/html-story-report/src/test/easyb/ZipCodeStory.groovy
+++
@@ -1,7 +0,0 @@
-scenario "invalid zip code", {
-    given "an invalid zip code", {}
-    and
-    given "the zipcodevalidator is initialized", {}
-    when "validate is invoked with the invalid zip code", {}
-    then "the validator instance should return false", {}
-}
