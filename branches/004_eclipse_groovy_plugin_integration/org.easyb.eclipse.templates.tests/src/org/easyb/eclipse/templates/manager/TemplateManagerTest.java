7cda4ed72a1ef145ab7e6df95cd424203648183d
darran.m.white <darran.m.white@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-10-06 10:50:10 +0000

    Updated tests for templates

--- branches/004_eclipse_groovy_plugin_integration/org.easyb.eclipse.templates.tests/src/org/easyb/eclipse/templates/manager/TemplateManagerTest.java
+++ branches/004_eclipse_groovy_plugin_integration/org.easyb.eclipse.templates.tests/src/org/easyb/eclipse/templates/manager/TemplateManagerTest.java
@@ -1,13 +1,30 @@
 package org.easyb.eclipse.templates.manager;
 
-import org.eclipse.jface.text.templates.Template;
-
 import junit.framework.TestCase;
+
+import org.easyb.eclipse.templates.processor.BehaviourTemplateProposal;
+import org.easyb.eclipse.test.tools.EditorTool;
+import org.easyb.eclipse.test.tools.ProjectTool;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.ui.IEditorPart;
 
 public class TemplateManagerTest extends TestCase{
 
 	private static final String GIVEN_TEMPLATE = "given";
-
+	private IEditorPart editor;
+	@Override
+	protected void setUp()throws Exception{
+		IFile file = 
+			ProjectTool.setupJavaProjectAndBehaviourFile("test.story","test",getStory());
+		
+		editor = EditorTool.openEditor(file);
+	}
+	
+	private String getStory(){
+		return "given \"blah blah\"{}";
+	}
 	public void testGetTemplates(){
 		Template[] templates = 
 			TemplateManager.getInstance().getTemplates();
@@ -44,5 +61,15 @@ public class TemplateManagerTest extends TestCase{
 	}
 	
 	
-	
+	public void testGetTemplateProposals(){
+
+		ITextViewer viewer = EditorTool.getTextViewer(editor);
+		
+		BehaviourTemplateProposal[] proposals = 
+			TemplateManager.getInstance().getTemplateProposals(viewer,1);
+		
+		//Should only find the "given" template
+		assertTrue(proposals.length==1);
+		assertEquals("given",proposals[0].getTemplateName());
+	}
 }
