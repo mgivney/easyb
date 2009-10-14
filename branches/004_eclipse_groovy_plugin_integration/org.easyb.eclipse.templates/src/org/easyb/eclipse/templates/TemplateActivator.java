ca3a0a66b0a102036bfc7da4bf074669ee54412b
darran.m.white <darran.m.white@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-10-14 13:21:36 +0000

    Proposal computer now filters the underlying resource and only makes suggestions for .story or .specification files

--- branches/004_eclipse_groovy_plugin_integration/org.easyb.eclipse.templates/src/org/easyb/eclipse/templates/TemplateActivator.java
+++ branches/004_eclipse_groovy_plugin_integration/org.easyb.eclipse.templates/src/org/easyb/eclipse/templates/TemplateActivator.java
@@ -1,7 +1,11 @@
 package org.easyb.eclipse.templates;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -9,7 +13,9 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class TemplateActivator extends AbstractUIPlugin {
-
+	private String STORY_CONTENT_TYPE = "org.easyb.eclipse.contenttype.story";
+	private String SPECIFICATION_CONTENT_TYPE = "org.easyb.eclipse.contenttype.specification";
+	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.easyb.eclipse.templates";
 
@@ -59,5 +65,38 @@ public class TemplateActivator extends AbstractUIPlugin {
 	
 	public static void Log(IStatus status){
 		getDefault().getLog().log(status);
+	}
+	
+	public boolean isBehaviourFile(IResource resource){
+		if(resource.getType() != IResource.FILE){
+			return false;
+		}
+		
+		IContentType type = 
+			Platform.getContentTypeManager().getContentType(STORY_CONTENT_TYPE);
+		
+		String resourceExt = ((IFile)resource).getFileExtension();
+		if(isFileExtensionMatch(type.getFileSpecs(IContentType.FILE_EXTENSION_SPEC),resourceExt)){
+			return true;
+		}
+		
+		type = 
+			Platform.getContentTypeManager().getContentType(SPECIFICATION_CONTENT_TYPE);
+
+		if(isFileExtensionMatch(type.getFileSpecs(IContentType.FILE_EXTENSION_SPEC),resourceExt)){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean isFileExtensionMatch(String[] extensions,String resourceExt){
+		for(String exten : extensions){
+			if(exten.equals(resourceExt)){
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
