4ea6c84786fb93f349fca07948bf2bb431d47756
jeffrey.erikson <jeffrey.erikson@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-01-08 00:02:31 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-0.9.6-2

--- eclipse-plugins/org.easyb.eclipse.launch.ui.tests/resources/behaviours/my_story.story
+++
@@ -1,2 +0,0 @@
-scenario "Test  scenario"{
-}
--- eclipse-plugins/org.easyb.eclipse.ui/src/org/easyb/ui/utils/IUIConstants.java
+++
@@ -1,6 +0,0 @@
-package org.easyb.ui.utils;
-
-public interface IUIConstants {
-	public static final String STORY_EXTENSION = "story";
-	public static final String SPECIFICATION_EXTENSION = "specification";
-}
