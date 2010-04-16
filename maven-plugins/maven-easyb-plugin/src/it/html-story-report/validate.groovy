b95addd6a57bff2e70b4bf474d1c0f50acfa2d7a
jeffrey.erikson <jeffrey.erikson@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-04-16 02:05:16 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-0.9.7-1

--- maven-plugins/maven-easyb-plugin/src/it/html-story-report/validate.groovy
+++
@@ -1,5 +0,0 @@
-storyReportPath = new File("${basedir}/target/easyb")
-assert storyReportPath.exists()
-
-storyReport = new File(storyReportPath , 'stories.html')
-assert storyReport.text.contains("<html")
